4150a57faae8b4fd114bc74a5cfad12f618122ef
Azure Pipelines <docfx@linq2db.com>
2026-05-17 23:23:52 +0000

    DocFX update by CI

---
+++ public/main.js
@@ -0,0 +1,10 @@
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/linq2db/linq2db',
+            title: 'Linq To DB'
+        }
+    ]
+}
+
